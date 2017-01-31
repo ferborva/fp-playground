7a301bc0d128560af78b95c3886b2bd98163f3fc
Bordallo, Fernando <fernando.bordallo@barco.com>
2017-01-31 16:13:08 +0100

    chore(): basic fooling around

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = 3000;
+
+// Define statics file
+app.use('/web', express.static(path.join(__dirname, 'webs')));
 
 app.get('/', (req, res) => {
   res.send('Hello world!');
